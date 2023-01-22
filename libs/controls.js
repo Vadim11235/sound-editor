d726903eaf54dc6202d3200ebb8a01a192e41a2a
Vadim <ave11235813@gmail.com>
2023-01-22 18:27:34 +0200

    init

---
+++ libs/controls.js
@@ -0,0 +1,31 @@
+let audioContext;
+
+window.addEventListener('mousedown', function(event) {
+	if(!event.target.classList.contains('js-control')) return;
+	if(!audioContext) {
+		audioContext = new (window.AudioContext || window.webkitAudioContext)();
+	}
+
+	// Создаём узел контроля громкости (усиления)
+	// const gainNode = audioContext.createGain();
+	// gainNode.gain.value = 1;
+
+	const folder = event.target.dataset.type;
+	const song = event.target.dataset.id;
+	if(!song) return;
+
+	const audio = new Audio(`samples/${folder}/${song}`);
+	audio.src = `samples/${folder}/${song}`;
+	audio.controls = true;
+	// audio.loop = true;
+	audio.autoplay = true;
+	document.body.appendChild(audio);
+
+	const source = audioContext.createMediaElementSource(audio);
+	source.connect(audioContext.destination);
+
+	// source.connect(gainNode);
+	// gainNode.connect(audioContext.destination);
+});
+
+
