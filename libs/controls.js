5a865251f794f26f35b0b4a222b0330b4bc63c60
Vadim <ave11235813@gmail.com>
2023-01-22 20:07:26 +0200

    added samples

--- libs/controls.js
+++ libs/controls.js
@@ -1,3 +1,14 @@
+/*
+file:///home/ave11235/%D0%94%D0%BE%D0%BA%D1%83%D0%BC%D0%B5%D0%BD%D1%82%D1%8B/Works/Experiments/%D0%9B%D1%83%D1%87%D1%88%D0%B8%D0%B5%20%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%D1%8B/js%20-%20%D0%9C%D0%B8%D0%BA%D1%88%D0%B5%D1%80%D0%BD%D1%8B%D0%B9%20%D0%BF%D1%83%D0%BB%D1%8C%D1%82/index.html
+https://developer.mozilla.org/ru/docs/Web/API/Web_Audio_API
+https://developer.mozilla.org/ru/docs/Web/API/AudioContext
+https://www.w3schools.com/tags/ref_av_dom.asp
+*/
+
+const APP = {
+	play: {}
+};
+const audio = new Audio();
 let audioContext;
 
 window.addEventListener('mousedown', function(event) {
@@ -6,26 +17,55 @@ window.addEventListener('mousedown', function(event) {
 		audioContext = new (window.AudioContext || window.webkitAudioContext)();
 	}
 
-	// Создаём узел контроля громкости (усиления)
-	const gainNode = audioContext.createGain();
-	gainNode.gain.value = 1;
-
 	const folder = event.target.dataset.type;
 	const song = event.target.dataset.id;
 	if(!song) return;
 
-	const audio = new Audio(`samples/${folder}/${song}`);
 	audio.src = `samples/${folder}/${song}`;
-	// audio.controls = true;
-	// audio.loop = true;
-	audio.autoplay = true;
-	// document.body.appendChild(audio);
+	// stop(audio.src);
+	stopAll();
+	play(audio.src, event.target);
+
+	// Создаём узел контроля громкости (усиления)
+	const gainNode = audioContext.createGain();
+	gainNode.gain.value = 1;
+
 
 	const source = audioContext.createMediaElementSource(audio);
 	// source.connect(audioContext.destination);
 
 	source.connect(gainNode);
 	gainNode.connect(audioContext.destination);
+
+	audio.play();
+});
+audio.addEventListener('ended', function() {
+	stop(this.src);
+});
+audio.addEventListener('timeupdate', function() {
+	APP.play[this.src].track.style.height = (audio.currentTime / audio.duration).toFixed(2) * 100 + '%';
 });
 
+function play(key, button) {
+	button.classList.add('active');
+	const track = document.createElement('div');
+	track.classList.add('time');
+	button.appendChild(track);
 
+	APP.play[key] = {
+		button, track
+	};
+}
+function stopAll() {
+	for(const k in APP.play) {
+		APP.play[k].button.classList.remove('active');
+		APP.play[k].track.remove();
+		delete APP.play[k];
+	}
+}
+function stop(key) {
+	if(!(key in APP.play)) return;
+	APP.play[key].button.classList.remove('active');
+	APP.play[key].track.remove();
+	delete APP.play[key];
+}
